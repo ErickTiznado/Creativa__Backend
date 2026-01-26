37c3929ad3d9aad3b87932cddfdb67f36fa17e8f
ErickTiznado <88002820+ErickTiznado@users.noreply.github.com>
2026-01-26 16:07:58 -0600

    feat: Implement campaign management API endpoints and logic, remove `@google/genai` dependency, and delete outdated documentation files.

--- src/controllers/campaigns.controller.js
+++ src/controllers/campaigns.controller.js
@@ -25,13 +25,94 @@ export const getCampaigns = async (req, res) => {
 };
 
 export const getCampaignsDesigners = async (req, res) => {
-  const { designerId } = req.body;
-
-  const response = await Brief.select().where("designer_id", designerId).get();
-
-  res.json({
-    message: "Campañas obtenidas con éxito",
-    data: response,
-    success: true,
-  });
+  const { designerId } = req.query;
+  if (designerId === undefined) {
+    return res.json({
+      message: "No se proporciono un id de diseñador",
+      success: false,
+    });
+  }
+  console.log(designerId);
+  try {
+    const response = await Brief.select().where("designer_id", designerId).get();
+    console.log(response);
+    res.json({
+      message: "Campañas obtenidas con éxito",
+      data: response,
+      success: true,
+    });
+  } catch (error) {
+    res.statusCode = 500;
+    res.json({
+      message: "Error al obtener las campañas",
+      error: error.message,
+      success: false,
+    });
+  }
 };
+
+
+
+export const updateStateCampaign = async (req, res) => {
+  const { campaignId, status } = req.body;
+
+  if ((campaignId.length === 0 || status.length === 0) || campaignId === undefined || status === undefined) {
+    res.statusCode = 400;
+    return res.json({
+      message: "No se proporciono un id de campaña o estado",
+      success: false,
+    });
+  }
+
+  try {
+    const response = await Brief.where("id", campaignId).update({
+      status: status
+    });
+
+    res.statusCode = 200;
+    res.json({
+      message: "Status actualizado con exito",
+      success: true,
+    });
+
+  } catch (error) {
+    res.statusCode = 500;
+    res.json({
+      message: "Error al actualizar el status",
+      error: error.message,
+      success: false,
+    });
+  }
+}
+
+
+export const getCampaingById = async (req, res) => {
+  const { campaignId, designerId } = req.query;
+  console.log(campaignId, designerId);
+  if (campaignId === undefined || designerId === undefined) {
+    res.statusCode = 400;
+    return res.json({
+      message: "No se proporciono un id de campaña o diseñador",
+      success: false,
+    });
+  }
+
+  try {
+
+    const response = await Brief.select().where("id", campaignId).where("designer_id", designerId).get();
+
+    res.statusCode = 200;
+    res.json({
+      message: "Ok",
+      data: response,
+      success: true
+    })
+  } catch (e) {
+    res.statusCode = 500;
+    res.json({
+      message: "Error al obtener la campaña",
+      error: e.message,
+      success: false,
+    })
+  }
+}
